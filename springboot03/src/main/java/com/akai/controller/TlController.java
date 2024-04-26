9ae28fc2da645cfe515762917d8e942a23e58627
loveshiho <1822881521@qq.com>
2024-04-26 18:56:59 +0800

    测试springboot——>整合Thymeleaf——>内置对象

--- springboot03/src/main/java/com/akai/controller/TlController.java
+++ springboot03/src/main/java/com/akai/controller/TlController.java
@@ -2,6 +2,8 @@ package com.akai.controller;
 
 import com.akai.pojo.Emp;
 import com.akai.service.EmpService;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,18 +21,30 @@ public class TlController {
         model.put("msg", "hello");
         return "index";
     }
+
     @RequestMapping("/showEmps")
     public String showEmps(Map<String, Object> model) {
+
         List<Emp> all = empService.findAll();
         model.put("emps", all);
-        model.put("emp",all.get(0));
+        model.put("emp", all.get(0));
         return "showEmps";
     }
+
     @RequestMapping("/removeEmp")
     public String removeEmp(Integer empno) {
         int i = empService.deleteEmpById(empno);
         return "forward:showEmps";
         // return "forward:showEmps.html";
     }
-
+    @RequestMapping("/showIndex")
+    public String showIndex(HttpServletRequest req, HttpSession session) {
+        // 向request域放数据
+        req.setAttribute("msg", "requestMessage");
+        // 向session域放数据
+        session.setAttribute("msg", "sessionMessage");
+        // 向application域放数据
+        req.getServletContext().setAttribute("msg", "applicationMessage");
+        return "index";
+    }
 }
