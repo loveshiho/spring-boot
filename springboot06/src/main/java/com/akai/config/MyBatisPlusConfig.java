b39b543ba6126b301fed9972b285c9b903a4e235
loveshiho <1822881521@qq.com>
2024-04-27 19:44:24 +0800

    测试springboot——>mybatisPlus——>crud + page

---
+++ springboot06/src/main/java/com/akai/config/MyBatisPlusConfig.java
@@ -0,0 +1,24 @@
+package com.akai.config;
+
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MyBatisPlusConfig {
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        MybatisPlusInterceptor mybatisPlusInterceptor = new MybatisPlusInterceptor();
+        PaginationInnerInterceptor paginationInnerInterceptor = new PaginationInnerInterceptor();
+        // 设置请求的页面大于最大页后操作，true调回到首页，false继续请求 默认false
+        // paginationInnerInterceptor.setOverflow(false);
+        // 设置最大单页限制数量，默认 500 条，-1 不受限制
+        // paginationInnerInterceptor.setMaxLimit(500L);
+        // 设置数据库类型
+        paginationInnerInterceptor.setDbType(DbType.MYSQL);
+        mybatisPlusInterceptor.addInnerInterceptor(paginationInnerInterceptor);
+        return mybatisPlusInterceptor;
+    }
+}
--- springboot06/src/test/java/com/akai/Springboot06ApplicationTests.java
+++ springboot06/src/test/java/com/akai/Springboot06ApplicationTests.java
@@ -3,6 +3,8 @@ package com.akai;
 import com.akai.pojo.Dept;
 import com.akai.service.DeptService;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -14,10 +16,66 @@ class Springboot06ApplicationTests {
 
     @Autowired
     public DeptService deptService;
+
     @Test
     public void testFindAll() {
         List<Dept> list = deptService.list();
         list.forEach(System.out::println);
     }
 
+    @Test
+    public void testQueryWrapper() {
+        QueryWrapper<Dept> qw = new QueryWrapper<>();
+        // QueryWrapper 作用就是在原本的 SQL语句后面拼接 where条件
+        qw.ge("deptno", 20).eq("dname", "ACCOUNTING").likeRight("dname", "A");
+        List<Dept> list = deptService.list(qw);
+        list.forEach(System.out::println);
+    }
+
+    @Test
+    public void testQueryWrapper2() {
+        QueryWrapper<Dept> qw = new QueryWrapper<>();
+        qw.eq("deptno", 20);
+        Dept dept = deptService.getOne(qw);
+        System.out.println(dept);
+    }
+
+    @Test
+    public void testAdd() {
+        deptService.save(new Dept(null, "Java", "xian"));
+    }
+
+    @Test
+    public void testUpdate() {
+        Dept dept = new Dept();
+        dept.setDname("Spring");
+        dept.setLoc("shiho");
+        // 更新的条件
+        QueryWrapper<Dept> qw = new QueryWrapper<>();
+        qw.eq("deptno", 44);
+        deptService.update(dept, qw);
+    }
+
+    @Test
+    public void testRemove() {
+        QueryWrapper<Dept> qw = new QueryWrapper<>();
+        qw.eq("deptno", 44);
+        deptService.remove(qw);
+    }
+
+    // 测试分页
+    @Test
+    public void testPage() {
+        // 页码数、页大小
+        QueryWrapper<Dept> qw = new QueryWrapper<>();
+        qw.likeRight("dname", "A");
+        Page<Dept> page = deptService.page(new Page<>(1, 2), qw);
+        // 当前页数据  总页数  总记录数  当前页  页大小 ... ..
+        List<Dept> records = page.getRecords();
+        records.forEach(System.out::println);
+        System.out.println("总页数:" + page.getPages());
+        System.out.println("总记录数:" + page.getTotal());
+        System.out.println("当前页:" + page.getCurrent());
+        System.out.println("页大小:" + page.getSize());
+    }
 }
